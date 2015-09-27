ef0dea6c968ebeafbbae8500e91d72c1fd241d2c
johmie <johmie@johwin>
2015-09-27 18:09:07 +0200

    Files cleaned up

--- survey/src/main/java/io/feedback/demo/TestSpringHibernateJpa.java
+++ survey/src/main/java/io/feedback/demo/TestSpringHibernateJpa.java
@@ -8,8 +8,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpringHibernateJpa {
     public static void main(String[] args) {
-        System.out.println("************** BEGINNING PROGRAM **************");
-
         ApplicationContext context = new ClassPathXmlApplicationContext(
                 "spring-config.xml");
         ProjectService projectService = (ProjectService) context
@@ -20,12 +18,7 @@ public class TestSpringHibernateJpa {
         project.setTitle("My title");
         projectService.addProject(project);
         System.out.println("Project : " + project + " added successfully");
-        
+
         System.out.println(projectService.findAllProjects().toString());
-
-//        List<Person> persons = personService.fetchAllPersons();
-//        System.out.println("The list of all persons = " + persons);
-
-        System.out.println("************** ENDING PROGRAM *****************");
     }
 }
